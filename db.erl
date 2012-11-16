af4ed229c27eea1db02327b3feb758c641e772b3
Dionysios <dpapathanopoulos@webst.gr>
2012-11-16 11:12:54 +0100

    Insert data and checking duplicates

--- db.erl
+++ db.erl
@@ -2,15 +2,25 @@
 -export([start/1]).
 
 % depend on the machine config
-
 -define(DSN, "test").
 -define(UID, "machete").
 -define(PWD, "machete@1991").
+-define(ConnectStr, "DSN=mysql;UID=machete;PWD=machete@1991").
 
-%Start a new ODBC server
-start([H1|T]) ->
-	{ok, Ref} = odbc:connect("DSN=mysql;UID=machete;PWD=machete@1991",[]),
-odbc:sql_query(Ref, "INSERT INTO wp_posts (post_author, 
+start([Place, Address, Name, Desc, Time, Date, Img_url]) ->
+%start()->
+    % Start a new ODBC server. The application must already be started.
+     odbc:start(),
+                                        
+    % Connect to the database.
+    {ok,Ref}  = odbc:connect(?ConnectStr,[]),
+
+    
+% We are quering a wordpress database so we use the wp_tables to have compatibility with the worpress.
+% That's why sometimes like this query that follows I have add additional fields which we don't use 
+% the wordpress is using 
+		
+	Insert_query = "INSERT INTO wp_posts (post_author,
 	post_date, 
 	post_date_gmt, 
 	post_content, 
@@ -34,23 +44,44 @@ odbc:sql_query(Ref, "INSERT INTO wp_posts (post_author,
 	comment_count) VALUES (	1,
 	'2012-11-01 22:05:53',
 	'2012-11-01 22:05:53', 
-	'test',
-	'"++H1++"', 
-	' ',
+	'"++Desc++"',
+	'"++Name++"', 
+	'"++Place++" ',
 	'publish', 
 	'open', 
 	'open',
 	'', 
 	'post_name', 
-	'to_ping', 
+	'"++Date++"', 
 	'pinged', 
 	'2012-11-01 22:05:53', 
 	'2012-11-01 22:05:53', 
 	'post_content_filtered', 
 	'0', 
-	'http://localhost/site/?p=12', 
+	'http://localhost/Machete/?p=12', 
 	0, 
 	'post',
 	'post_mime_type', 
-	0)
-").
+	0)",
+
+	%% Since the parsers are always running we need an asynchronous check method to eliminate duplicates
+	%% I first query the database with a unique compination of the event arguments to see if it has already saved.
+	%% If not then I insert th new event to the database
+
+    SelectStmt = "SELECT id FROM wp_posts WHERE post_title='"++Name++"' AND post_excerpt = '"++Place++"' AND to_ping ='"++Date++"'",
+    Sql1 = odbc:sql_query(Ref, SelectStmt),
+    io:format("execute_stmt Select statement returns ~p~n",
+             [Sql1]),
+		case Sql1 of 
+    			Sql1 = {selected,["id"],[]}->
+				Sqd3  =	odbc:sql_query(Ref, Insert_query);
+			Sql1 = {selected,["id"],[R]} ->
+				io:format("Already exists")
+			end,   
+
+
+	%Disconnect
+	odbc:disconnect(Ref),
+	
+	% Stop the server
+	odbc:stop(). 
